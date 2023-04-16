d3c02dd1df65d33b808a3fd8b9f7189790a87cd7
Alexandre Amado <alexandreamado71@gmail.com>
2023-04-16 08:49:34 +0200

    files

--- Nodejs-Passport-Login-System/server.js
+++ Nodejs-Passport-Login-System/server.js
@@ -1,7 +1,17 @@
 const express = require('express') 
 const app = express()
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+
+
+const inicializePassport = require('./password-config')
+const passport = require('passport')
+inicializePassport(passport)
+
+const users = []
 
 app.set('view-engine', 'ejs')
+app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req, res) => {
     res.render('index.ejs', {name: "Alexandre"})
@@ -9,13 +19,31 @@ app.get('/', (req, res) => {
 
 app.get('/login', (req, res) => {
     res.render('login.ejs')
-}) 
+})  
+
+app.get('/login', (req, res) => {
+
+})
+
 
 app.get('/register', (req, res) => {
     res.render('register.ejs')
 })
 
-app.post('/register', (req, res) => {
+app.post('/register', async(req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        })
+        res.redirect('/login')
+    } catch {
+        res.redirect('/register')
+    }
+
     
 })
 
